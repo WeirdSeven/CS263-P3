d2a1f7ed4a67a62c0add55b57a6ee746e9bbb8a7
SodaGreen <hb4vf@virginia.edu>
2016-11-14 15:36:17 -0500

    fix11

--- logger.h
+++ logger.h
@@ -11,7 +11,7 @@ void log_headers(struct hdrs *headers) {
 	//struct ether_addr ether_dst_addr;
 	//ether_src_addr.ether_addr_octet = (u_int8_t[6])ethernet_header->ether_src_addr;
 	//ether_dst_addr.ether_addr_octet = (u_int8_t[6])ethernet_header->ether_dst_addr;
-	printf("ETHERNET: src[%s] dst[%s]", ether_ntoa((struct ether_addr*)ether_header[6]), ether_ntoa((struct addr*)ethernet_header[0]));
+	printf("ETHERNET: src[%s] dst[%s]", ether_ntoa((struct ether_addr*)&ethernet_header[6]), ether_ntoa((struct addr*)&ethernet_header[0]));
 	//printf("ETHERNET: src[0c:df:27:16:b8:30] dst[50:5a:00:12:35:02]");
 }
 
